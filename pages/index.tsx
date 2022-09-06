49f08fc77b4894b36679971413d89719b7aae4c3
Andrey Filippov <androsphilippos@gmail.com>
2022-09-06 21:55:31 +0500

    feat: add MUI lib and chart, create DoughnutChart

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,33 @@
 import type { NextPage } from 'next';
 
+import { Container, Stack, Typography } from '@mui/material';
+import DoughnutChart from '../components/DoughnutChart/DoughnutChart';
+
 const Home: NextPage = () => {
-  return <></>;
+  return (
+    <Container
+      maxWidth='lg'
+      sx={{
+        height: '100vh',
+        backgroundImage: 'linear-gradient(to top, #dfe9f3 0%, white 100%)',
+      }}
+    >
+      <Stack
+        spacing={4}
+        alignItems='center'
+      >
+        <Typography
+          variant='h1'
+          align='center'
+        >
+          Free Juniors
+        </Typography>
+        <Container maxWidth='sm'>
+          <DoughnutChart />
+        </Container>
+      </Stack>
+    </Container>
+  );
 };
 
 export default Home;
