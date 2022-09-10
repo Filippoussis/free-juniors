6602f18b95051bda6cbcaa78e67286270b9661f1
Andrey Filippov <androsphilippos@gmail.com>
2022-09-10 11:24:16 +0500

    fix: create plugins for DoughnutChart

--- components/DoughnutChart/DoughnutChart.tsx
+++ components/DoughnutChart/DoughnutChart.tsx
@@ -4,6 +4,39 @@ import { Doughnut } from 'react-chartjs-2';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
+const legendMargin = {
+  id: 'legendMargin',
+  afterDatasetsDraw(chart: any) {
+    const { ctx, data } = chart;
+    ctx.save();
+
+    const xCenter = chart.getDatasetMeta(0).data[0].x;
+    const yCenter = chart.getDatasetMeta(0).data[0].y;
+
+    let total = 0;
+    const dataValues = data.datasets[0].data;
+    if (dataValues.length > 0) {
+      total = dataValues.reduce((acc: any, curr: any) => acc + curr);
+    }
+
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.font = 'bold 36px Roboto, Helvetica, Aria, sans-serif';
+    ctx.fillText(total, xCenter, yCenter);
+  },
+};
+
+const textCenter = {
+  id: 'textCenter',
+  beforeInit(chart: any) {
+    const fitValue = chart.legend.fit;
+    chart.legend.fit = function () {
+      fitValue.bind(chart.legend)();
+      return (this.height += 24);
+    };
+  },
+};
+
 const data = {
   labels: ['JS', 'Python', 'Java', 'C#', 'PHP', 'Ruby'],
   datasets: [
@@ -30,6 +63,11 @@ const data = {
   ],
 };
 
-const DoughnutChart = () => <Doughnut data={data} />;
+const DoughnutChart = () => (
+  <Doughnut
+    data={data}
+    plugins={[legendMargin, textCenter]}
+  />
+);
 
 export default DoughnutChart;
